4c707a62ed53a6680570afa0d03e61cdcfaa93a6
BeepBopBeep <qyburgstark@gmail.com>
2021-06-06 00:57:51 +0530

    Making Image Gallery and it's effect

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 document.querySelector(".arrow-up").addEventListener('click',()=>{
     document.getElementById("about").scrollIntoView();
 })
+// Adding multiple mock images in gallery
+galleryObj = document.querySelectorAll(".gallery-object")[0];
+for(let i=0;i<10;i++){
+    galleryObjElement = galleryObj.cloneNode(true);
+    document.querySelector(".gallery").appendChild(galleryObjElement);
+}
