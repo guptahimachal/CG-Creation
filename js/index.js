0da1a1b23e8680aa5a07e5e53e328df3db847cd6
BeepBopBeep <qyburgstark@gmail.com>
2021-06-07 12:11:11 +0530

    Adding Horizontal Bar on onload as default was causing problem in calculating width

--- js/index.js
+++ js/index.js
@@ -15,27 +15,31 @@ for(let i=0;i<TotalImages;i++){
 }
 
 
+
 // ADDING THE RIGHT HORIZONTAL BAR WITH RESPONSIVE LENGTH
 // according to image title
-// Done by appendin a new style sheet
-var style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = ' ';
+// Done by appendin a new style sheet    
+function addHorizontalBar() {
+    alert('ok');
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = ' ';
 
-// Width of the gallery object
-// Our required length will be = (W - 2*20 - 40 - 5 - t )px
-// W : galleryObjWidth
-// 2*20 : for (thin-line) border width 
-//   40 : Image h5 is 40px from that thin line 
-//    5 : 5px gap on the right of h5 and line 
-//    t : width of h5
-galleryObjWidth = galleryObj.clientWidth;
-imageTitles = document.querySelectorAll(".image-title h5");
-imageTitles.forEach((element,i) => {
-    style.innerHTML += `.image-title h5:nth-child(${i+1})::after{
-        width: ${galleryObjWidth-85-element.clientWidth}px;
-    }
-    `
+    // Width of the gallery object
+    // Our required length will be = (W - 2*20 - 40 - 5 - t )px
+    // W : galleryObjWidth
+    // 2*20 : for (thin-line) border width 
+    //   40 : Image h5 is 40px from that thin line 
+    //    5 : 5px gap on the right of h5 and line 
+    //    t : width of h5
+    galleryObjWidth = galleryObj.clientWidth;
+    imageTitles = document.querySelectorAll(".image-title h5");
+    imageTitles.forEach((element,i) => {
+        style.innerHTML += `.image-title h5:nth-child(${i+1})::after{
+            width: ${galleryObjWidth-85-element.clientWidth}px;
+        }`
 });
 // Finally adding the prepared CSS
 document.getElementsByTagName('head')[0].appendChild(style);
+}
+window.onload = addHorizontalBar;
